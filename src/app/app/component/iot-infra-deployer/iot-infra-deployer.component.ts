237300824c25c88a15a179622c0131d59cd88c8b
marcelo <gallardo@tibco.com>
2021-07-14 13:34:50 -0400

    Initial Revision

---
+++ src/app/app/component/iot-infra-deployer/iot-infra-deployer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iot-infra-deployer',
+  templateUrl: './iot-infra-deployer.component.html',
+  styleUrls: ['./iot-infra-deployer.component.css']
+})
+export class IotInfraDeployerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
