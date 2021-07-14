24f8d43e96646d1a436888cd4bd612fd04e3962e
magallardo <gallardo@tibco.com>
2021-07-14 15:37:13 -0400

    Added logic to infra deployer

--- src/app/components/iot-infra-deployer/iot-infra-deployer.component.ts
+++ src/app/components/iot-infra-deployer/iot-infra-deployer.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+
+import { GraphService } from '../../services/graph/graph.service';
+import { FlogoDeployService } from '../../services/deployment/flogo-deploy.service';
+import { Gateway } from '../../shared/models/iot.model';
+
+export interface SelectItem {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-iot-infra-deployer',
   templateUrl: './iot-infra-deployer.component.html',
@@ -7,9 +20,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IotInfraDeployerComponent implements OnInit {
 
-  constructor() { }
+  gatewayId = "";
+  gateway = null as Gateway;
+  deployerForm: FormGroup;
+
+  deployerTypes: SelectItem[] = [
+    { value: 'AIR', viewValue: 'AIR Deployer' },
+    { value: 'OH', viewValue: 'OpenHorizon' }
+  ];
+
+  constructor(private graphService: GraphService,
+    private flogoDeployService: FlogoDeployService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar) { 
+
+    }
 
   ngOnInit(): void {
+    // Get gateway id from route
+    this.gatewayId = this.route.snapshot.paramMap.get('gatewayId');
+
+    this.getGatewayDetails(this.gatewayId);
+
+  }
+
+  /**
+   * Get Gateway, Pipelines and Devices information
+   */
+   public getGatewayDetails(gatewayId: string) {
+    console.log("Getting gateway and pipelines for: ", gatewayId);
+
+    this.graphService.getGatewayAndPipelines(gatewayId)
+      .subscribe(res => {
+        console.log("Received response for graphService.getGatewayAndPipelines: ", res);
+        this.gateway = res[0] as Gateway;
+
+
+      })
+  }
+
+  createForm() {
+
+    this.deployerForm = this.formBuilder.group({
+      deployerType: ['', Validators.required],
+      deployEdgex: [true, Validators.required],
+      deployAIRBase: [true, Validators.required],
+    });
+
+  }
+
+  deploy() {
+
+  }
+
+  undeploy() {
+
   }
 
 }
