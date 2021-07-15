873d92f15eec4507296ad12b28a7b988ead9c0f1
magallardo <gallardo@tibco.com>
2021-07-15 09:26:35 -0400

    Updated form with new fields

--- src/app/components/iot-infra-deployer/iot-infra-deployer.component.ts
+++ src/app/components/iot-infra-deployer/iot-infra-deployer.component.ts
@@ -52,37 +52,37 @@ export class IotInfraDeployerComponent implements OnInit {
   createForm() {
 
     this.deployerForm = this.formBuilder.group({
-      deployerType: ['', Validators.required],
+      deployerType: ['OH', Validators.required],
       deployEdgex: [true, Validators.required],
       deployAIRBase: [true, Validators.required],
-      deployerURL: ['http://34.232.71.154:3090/v1', Validators.required],
-      deployConstraints: ['', Validators.required],
-      gatewayId: ['', Validators.required],
-      gatewayDescription: ['', Validators.required],
-      gatewayHostname: ['', Validators.required],
-      gatewayRouter: ['http://34.232.71.154:3090/v1', Validators.required],
+      deployerURL: ['http://54.88.210.184:3090/v1', Validators.required],
+      deployConstraints: ['[\"role == RTSF_Demo\"]', Validators.required],
+      gatewayId: ['Ubuntu18_IoT', Validators.required],
+      gatewayDescription: ['Devices and Sensors for point of sales station', Validators.required],
+      gatewayHostname: ['192.168.1.99', Validators.required],
+      gatewayRouter: ['http://54.88.210.184:3090/v1', Validators.required],
       gatewayRouterPort: ['22', Validators.required],
-      gatewayDeployNetwork: ['', Validators.required],
+      gatewayDeployNetwork: ['loss-detection-app_edgex-network', Validators.required],
       gatewayLatitude: ['33.306340', Validators.required],
       gatewayLongitude: ['-111.934330', Validators.required],
-      gatewayAccessToken: ['', Validators.required],
+      gatewayAccessToken: ['NONE', Validators.required],
       gatewayUsername: ['admin:NRtypkkV2fO514hZcvVz0Z194AhiPz@myorg', Validators.required],
       gatewayPlatform: ['linux/amd64', Validators.required],
       // gatewayMetadataClient: ['http://edgex-core-metadata:48081', Validators.required],
       gatewayMetadataClient: ['http://localhost:48081', Validators.required],
       gatewayMetadataPublishIntervalSecs: ['30', Validators.required],
-      airMqttHosname: ['', Validators.required],
+      airMqttHosname: ['ab96cf877a138460588b658196b4c430-1973527714.us-west-2.elb.amazonaws.com', Validators.required],
       airMqttPort: ['443', Validators.required],
       airMqttUser: ['mqtt_admin', Validators.required],
       airMqttPassword: ['mqtt_admin', Validators.required],
       airMqttDataTopic: ['EdgexGatewayData', Validators.required],
-      airMqttDataMetadataPublisher: ['Ubuntu18_IoTEdgexDataMetadataClient', Validators.required],
+      airMqttDataMetadataPublisher: ['U18SnapEdgex_IoTEdgexDataMetadataClient', Validators.required],
       edgeMqttHostname: ['localhost', Validators.required],
       edgeMqttPort: ['1883', Validators.required],
       edgeMqttUser: ['', Validators.required],
       edgeMqttPassword: ['', Validators.required],
       edgeMqttDataTopic: ['edgex', Validators.required],
-      edgeMqttZmqToMqttPublisher: ['Ubuntu18_IoTEdgeZMQToMQTTClient', Validators.required],
+      edgeMqttZmqToMqttPublisher: ['U18SnapEdgex_IoTEdgeZMQToMQTTClient', Validators.required],
     });
 
   }
@@ -161,20 +161,20 @@ export class IotInfraDeployerComponent implements OnInit {
     console.log("DeployRequest: ", deployRequest);
     console.log("Deploy Request string: ", JSON.stringify(deployRequest));
     
-    // this.flogoDeployService.deployInfra(deployRequest)
-    //   .subscribe(res => {
-    //     console.log("Received Deployment response: ", res);
+    this.flogoDeployService.deployInfra(deployRequest)
+      .subscribe(res => {
+        console.log("Received Deployment response: ", res);
 
-    //     let message = 'Success';
-    //     if (res == undefined || res.Success == false) {
-    //       message = 'Failure';
-    //     }
+        let message = 'Success';
+        if (res == undefined || res.Success == false) {
+          message = 'Failure';
+        }
 
-    //     this._snackBar.open(message, "Deploy Pipeline", {
-    //       duration: 3000,
-    //     });
+        this._snackBar.open(message, "Deploy Pipeline", {
+          duration: 3000,
+        });
 
-    //   });
+      });
 
   }
 
@@ -193,6 +193,8 @@ export class IotInfraDeployerComponent implements OnInit {
       "ScriptSystemEnv": systemEnv,
       "UserDefinedParameters": {}
     };
+
+    console.log("UndeployRequest: ", undeployRequest);
 
     this.flogoDeployService.undeployInfra(undeployRequest)
       .subscribe(res => {
