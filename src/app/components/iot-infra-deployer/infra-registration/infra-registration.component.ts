67a828b159b4409cf93b15bdde89aa1fb130f24a
Syrine Matoussi <smatoussi@Syrines-MacBook-Pro.local>
2021-07-28 16:49:22 -0400

    added containers and their paramters in the registration tab

--- src/app/components/iot-infra-deployer/infra-registration/infra-registration.component.ts
+++ src/app/components/iot-infra-deployer/infra-registration/infra-registration.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as YAML from 'yaml'
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
 
 @Component({
   selector: 'app-infra-registration',
@@ -6,10 +12,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infra-registration.component.css']
 })
 export class InfraRegistrationComponent implements OnInit {
+  registrationForm: FormGroup;  
+  parsedFile:any;
+  items=[];
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder, private _snackBar: MatSnackBar) {
+
+   }
 
   ngOnInit(): void {
+    this.createForm();
+    }
+
+    onFileSelected(event) {
+      //parse yml file
+
+      const file: File = event.target.files[0];
+  
+      if (file) {
+  
+        console.log("Selected file: ", file.name);
+  
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          console.log("File content: ", fileReader.result);
+          this.parsedFile=YAML.parse(fileReader.result as string);
+        //add containers 
+          for (const key in this.parsedFile.services){
+            this.items.push([this.parsedFile.services[key].container_name,this.parsedFile.services[key].environment])
+         }
+            
+        }
+        fileReader.readAsText(file);    
+      }
+
+
+    }
+  //add container parameters
+  addform(item){
+    this.getParameters().clear();
+       for (let param in item[1]){
+           this.addParameterItem(param);}
+
+  }
+  //make dynamic forms
+  createForm() {
+    console.log("Creating form");
+    this.registrationForm = this.formBuilder.group({
+      containerName: ['', Validators.required],
+      parameters: this.formBuilder.array([],Validators.required)
+    });
+
+  }
+  createParameter(name): FormGroup {
+    console.log("Creating form with param name: ", name);
+    
+    return this.formBuilder.group({
+      name: [name, Validators.required],
+      value: ['', Validators.required]
+    });
   }
 
+  getParameters(): FormArray {
+    return <FormArray> this.registrationForm.get('parameters');
+  }
+
+  addParameterItem(name) {
+    this.getParameters().push(this.createParameter(name));
+  }
+//registration
+  save(){
+
+  }
+
+  register(){
+
+  }
 }
