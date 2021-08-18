abd244e1a6337c09236ab200c23ac5136cf33c35
Syrine Matoussi <smatoussi@Syrines-MacBook-Pro.local>
2021-08-18 12:35:14 -0400

    cleaned up unused code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   dgraphBasicAuth: 'Basic YWRtaW46YWRtaW4=',
   f1EndpointUrl: 'http://localhost:8043/http://52.22.89.56:5408',
   airEndpointUrl: 'http://a810bf34250614537b70a9076266fc1b-801809582.us-west-2.elb.amazonaws.com',
+  lightcraneEndpointUrl: 'http://localhost:8043/http://52.22.89.56:5408',
   remoteGatewayUrl: 'http://localhost:8043',
   localGatewayUrl: 'https://localhost:8443'
 };
