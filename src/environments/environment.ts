573c3e2956ea4836e7972fd1d6089dd262f760d5
magallardo <gallardo@tibco.com>
2021-08-31 14:01:00 -0400

    Added variables for Textile demo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,15 @@
 
 export const environment = {
   production: false,
-  dgraphUrl: 'http://a810bf34250614537b70a9076266fc1b-801809582.us-west-2.elb.amazonaws.com/dgraph-alpha-http',
+  dgraphUrl: 'http://ab4fc9335d74c42ed909d6b036e876f6-237663437.us-west-2.elb.amazonaws.com/dgraph-alpha-http',
   dgraphBasicAuthEnabled: true,
   dgraphBasicAuth: 'Basic YWRtaW46YWRtaW4=',
   f1EndpointUrl: 'http://localhost:8043/http://52.22.89.56:5408',
-  airEndpointUrl: 'http://a810bf34250614537b70a9076266fc1b-801809582.us-west-2.elb.amazonaws.com',
+  airEndpointUrl: 'http://ab4fc9335d74c42ed909d6b036e876f6-237663437.us-west-2.elb.amazonaws.com',
   lightcraneEndpointUrl: 'http://localhost:8043/http://52.22.89.56:5408',
+  storeSimulatorEndpoingUrl: 'http://localhost:8043/http://54.242.47.1:9090',
+  textileSimulatorEndpoint1Url: 'http://localhost:8043/http://52.7.96.87:8090',
+  textileSimulatorEndpoint2Url: 'http://localhost:8043/http://52.7.96.87:9090',
   remoteGatewayUrl: 'http://localhost:8043',
   localGatewayUrl: 'https://localhost:8443'
 };
