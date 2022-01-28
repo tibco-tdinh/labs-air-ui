15462ec6cf5fffdb97361f853b10749c113290e7
tibco-tdinh <tdinh@tibco.com>
2022-01-28 10:14:52 -0600

    infra(github-actions): adjust linting and coverage report directories

--- cloud/karma.conf.js
+++ cloud/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ProjectAir'),
+      dir: require('path').join(__dirname, '../coverage/ProjectAir/cloud'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- on-prem/karma.conf.js
+++ on-prem/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ProjectAir'),
+      dir: require('path').join(__dirname, '../coverage/ProjectAir/on-prem'),
       subdir: '.',
       reporters: [
         { type: 'html' },
